2b7d09b909a765a8fa756c98005ebe1a64407be2
Stepan Miroshin <microshine@peculiar-02.home>
2016-01-18 13:19:35 -0800

    JWK: AES export

--- test/config.js
+++ test/config.js
@@ -1,7 +1,9 @@
 process.env['SOFTHSM2_CONF'] = '/etc/softhsm2.conf';
 
 module.exports = {
-	lib: "/usr/safenet/lunaclient/lib/libCryptoki2_64.so",
+	//lib: "/usr/safenet/lunaclient/lib/libCryptoki2_64.so",
+	lib: "/usr/local/lib/softhsm/libsofthsm2.so",
 	libName: "Luna 5",
-	pin: "FLH6-X6Gb-/CSL-YtAY",
+	//pin: "FLH6-X6Gb-/CSL-YtAY",
+	pin: "12345",
 }
