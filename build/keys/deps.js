2cdb4fb720cb6bc22bd30791bc8ab60cd043c30b
microshine <microshine@mail.ru>
2016-08-14 00:15:34 +0300

    remove compiled files

--- build/keys/deps.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("../core"));
-__export(require("../object"));
-__export(require("../session"));
-exports.ERROR_NOT_IMPLEMENTED = "Method is not implemented";
