2d32ad3f2fb0f24e25823b019fc0bc1deb71b639
microshine <microshine@mail.ru>
2016-08-14 03:24:19 +0300

    Update

--- .travis.linux.sh
+++ .travis.linux.sh
@@ -8,8 +8,13 @@ sudo apt-get install autoconf -y
 sudo apt-get install automake -y
 sudo apt-get install libtool -y
 
+echo "Configure SoftHSMv2"
 sh ./autogen.sh
 ./configure
 make
+echo "Install SoftHSMv2"
 sudo make install
+
+echo "Running SoftHSMv2"
+/usr/local/bin/softhsm2-util --init-token --so-pin "12345" --pin "12345" --slot 0 --label "My slot 0"
 cd ..
