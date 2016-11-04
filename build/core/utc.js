ed8bcc0e3478645bed817f28d5daa8e0e70f624c
microshine <microshine@mail.ru>
2016-11-04 14:47:35 +0300

    remove "build" folder

--- build/core/utc.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function dateFromString(text) {
-    var reg = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
-    var vals = reg.exec(text);
-    if (!vals)
-        return null;
-    return new Date(vals[1] + "-" + vals[2] + "-" + vals[3] + " " + vals[4] + ":" + vals[5] + ":" + vals[6] + ":" + vals[7]);
-}
-exports.dateFromString = dateFromString;
